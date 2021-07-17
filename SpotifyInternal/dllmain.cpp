e447ef9efa324b79a359683e87afa5f523ede39a
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-07-17 22:36:25 +0300

    Proje dosyası ekle.

---
+++ SpotifyInternal/dllmain.cpp
@@ -0,0 +1,19 @@
+﻿#include <Windows.h>
+
+void __stdcall main()
+{
+	// ...
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hModule);
+
+		if (!wcsstr(GetCommandLine(), L"--type="))
+			CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>( main ), nullptr, 0, nullptr);
+	}
+	return TRUE;
+}
+
---
+++ SpotifyInternal/framework.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Nadiren kullanılan verileri Windows üst bilgilerinden dışla
+// Windows Üst Bilgi Dosyaları
+#include <windows.h>
