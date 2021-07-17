6c9d17ac18845010658e9755f292e46af6958f2c
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-07-17 22:49:01 +0300

    Add Includes

--- SpotifyInternal/dllmain.cpp
+++ SpotifyInternal/dllmain.cpp
@@ -1,4 +1,10 @@
-﻿#include <Windows.h>
+﻿#include <WinSock2.h>
+#include <Windows.h>
+#include <iostream>
+#include <algorithm>
+#include <Psapi.h>
+#include <uniHook.hpp>
+#include <string>
 
 void __stdcall main()
 {
@@ -14,6 +20,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		if (!wcsstr(GetCommandLine(), L"--type="))
 			CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>( main ), nullptr, 0, nullptr);
 	}
+
 	return TRUE;
 }
 
