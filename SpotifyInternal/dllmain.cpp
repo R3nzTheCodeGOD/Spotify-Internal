e61fc3e09794f7b15597a88c62780ef800a727bd
R3nzTheCodeGOD <r3nzthecodegod@gmail.com>
2021-07-23 23:22:51 +0300

    increase code optimization

--- SpotifyInternal/dllmain.cpp
+++ SpotifyInternal/dllmain.cpp
@@ -143,7 +143,7 @@ int __stdcall main()
 	return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		DisableThreadLibraryCalls(hModule);
