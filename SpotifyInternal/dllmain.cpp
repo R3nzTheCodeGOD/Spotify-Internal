c005e3c415689eaec86aeb524d015f4f783d8484
r3nzthecodegod <r3nzthecodegod@gmail.com>
2021-10-05 13:07:13 +0300

    update

--- SpotifyInternal/dllmain.cpp
+++ SpotifyInternal/dllmain.cpp
@@ -1,7 +1,7 @@
 ﻿#include <Windows.h>
 #include <Psapi.h>
 
-bool __cdecl DataCompare(char* pData, const char* bSig, const char* szMask) noexcept
+bool DataCompare(char* pData, const char* bSig, const char* szMask) noexcept
 {
 	for (; *szMask; ++szMask, ++pData, ++bSig) {
 		if (*szMask == 'x' && *pData != *bSig)
@@ -10,11 +10,11 @@ bool __cdecl DataCompare(char* pData, const char* bSig, const char* szMask) noex
 	return (*szMask) == NULL;
 }
 
-char* __cdecl FindPattern(char* dwAddress, DWORD dwSize, const char* pbSig, const char* szMask) noexcept
+char* FindPattern(char* dwAddress, DWORD dwSize, const char* pbSig, const char* szMask) noexcept
 {
-	DWORD length = strlen(szMask);
+	DWORD length{ strlen(szMask) };
 
-	for (DWORD i = NULL; i < dwSize - length; i++) {
+	for (DWORD i{ 0 }; i < dwSize - length; ++i) {
 		__try {
 			if (DataCompare(dwAddress + i, pbSig, szMask))
 				return dwAddress + i;
@@ -26,18 +26,13 @@ char* __cdecl FindPattern(char* dwAddress, DWORD dwSize, const char* pbSig, cons
 	return 0;
 }
 
-char* __cdecl scan(const char* pattern, const char* mask, MODULEINFO m) noexcept
+char* scan(const char* pattern, const char* mask, MODULEINFO m) noexcept
 {
-	const auto hModule = GetModuleHandle(NULL);
+	const auto hModule{ GetModuleHandleA(nullptr) };
 	return FindPattern((char*)hModule, m.SizeOfImage, pattern, mask);
 }
 
-void __stdcall userMsg() noexcept
-{
-	MessageBoxA(nullptr, "Hook Successful", "SpotifyInternal", MB_OK | MB_ICONINFORMATION);
-}
-
-void __stdcall removeAds(char* fn) noexcept
+void removeAds(char* fn) noexcept
 {
 	DWORD oldProtect;
 
@@ -50,36 +45,36 @@ void __stdcall removeAds(char* fn) noexcept
 	VirtualProtect(fn + 6, 1, oldProtect, &oldProtect);
 }
 
-int __stdcall main() noexcept
+void APIENTRY main() noexcept
 {
-	const auto hModule = GetModuleHandle(NULL);
+	const auto hModule{ ::GetModuleHandleA(nullptr) };
 	MODULEINFO mInfo = { 0 };
 
-	if (GetModuleInformation(GetCurrentProcess(), hModule, &mInfo, sizeof(MODULEINFO))) {
-		auto skipPod = scan("\x83\xC4\x08\x84\xC0\x0F\x84\xE5\x03\x00\x00", "xxxxxxxxxxx", mInfo);
+	if (::K32GetModuleInformation(::GetCurrentProcess(), hModule, &mInfo, sizeof(MODULEINFO))) {
+		auto skipPod = scan("\x6C\xFF\xFF\xFF\x07\x0F\x85\xB7\x02\x00\x00", "xxxxxxxxxxx", mInfo);
 
 		if (skipPod) {
 			removeAds(skipPod);
-			userMsg();
+			::MessageBoxA(nullptr, "Hook Successful", "SpotifyInternal", MB_OK | MB_ICONINFORMATION);
 		} else {
 			skipPod = scan("\x83\xC4\x08\x84\xC0\x0F\x84\xED\x03\x00\x00", "xxxxxxxxxxx", mInfo);
 			
 			if (skipPod) {
 				removeAds(skipPod);
-				userMsg();
+				::MessageBoxA(nullptr, "Hook Successful", "SpotifyInternal", MB_OK | MB_ICONINFORMATION);
 			}
 		}
 	}
-
-	return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-		DisableThreadLibraryCalls(hModule);
+	if (reason == DLL_PROCESS_ATTACH) {
+		::DisableThreadLibraryCalls(hModule);
 		if (!wcsstr(GetCommandLine(), L"--type="))
-			CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(main), nullptr, 0, nullptr);
+			if (const auto hThread{ ::CreateThread(nullptr, 0u, (LPTHREAD_START_ROUTINE)main, nullptr, 0ul, nullptr) }; hThread)
+				::CloseHandle(hThread);
+
 	}
 
 	return TRUE;
