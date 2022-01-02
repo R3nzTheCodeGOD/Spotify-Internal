307694fc1d84f6d3b9f41d67c8bec4ce273f20fc
r3nzthecodegod <r3nzthecodegod@gmail.com>
2022-01-02 16:01:05 +0300

    update installer.bat

--- scripts/installer.bat
+++ scripts/installer.bat
@@ -1,38 +1,4 @@
 @echo off
-color 6
-cls
-SET original="%appdata%\Spotify\chrome_elf.dll"
-SET original_backup="%appdata%\Spotify\_chrome_elf.dll"
-SET modified="%cd%\chrome_elf.dll"
-
-taskkill /F /IM Spotify.exe
-
-If not exist %original% (
-	echo [R3nzError] not found spotify folder
-    GOTO end_code
-)
-
-If not exist %modified% (
-	echo [R3nzError] modified file not found
-    GOTO end_code
-)
-
-If exist %original_backup% (
-
-	echo original already exists in backup
-
-    echo f | xcopy /y /f %modified% %original%
-
-    echo [R3nzSuccess] successful installing
-    
-    GOTO end_code
-)
-
-echo f | xcopy /y /f %original% %original_backup%
-
-echo f | xcopy /y /f %modified% %original%
-
-echo [R3nzSuccess] successful installing
-
-:end_code
-pause > nul
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/R3nzTheCodeGOD/Spotify-Internal/master/scripts/install.ps1' | Invoke-Expression}"
+pause
+exit
